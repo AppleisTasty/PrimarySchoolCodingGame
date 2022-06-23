6c0b2f07ae0d3836667aee48923d56eecb1c9c62
AppleisTasty <fhszxxxllj@163.com>
2022-06-23 23:19:48 +0100

    dragndrop first accomplished

---
+++ Assets/CodeBox.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CodeBox : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("OnDrop");
+        //eventData.pointerDrag is the aim object that is being currently dragges
+        if( eventData.pointerDrag != null)
+        {
+            //make the item snap into the position as we dropped
+            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/DragnDrop.cs
+++ Assets/DragnDrop.cs
@@ -3,27 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class DragnDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
+public class DragnDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler, IDropHandler
 {
     private RectTransform rectTransform;
-    private void Awake()
+    [SerializeField] private Canvas canvas;
+    private CanvasGroup canvasGroup;
+
+
+    private void Awake() //executes when the script instance is loaded
     {
         rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
     }
+
     public void OnBeginDrag(PointerEventData eventData)
     {
         Debug.Log("OnBeginDrag");
+        canvasGroup.alpha = 0.6f;
+        canvasGroup.blocksRaycasts = false; //disable the listener on this instance
     }
 
     public void OnDrag(PointerEventData eventData)
     {
         Debug.Log("OnDrag");
-        rectTransform.anchoredPosition += eventData.delta; // this contains the movement delta
+        // make the object move alongside the mouse
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor; // in case the canvas scales it still follows the mouse
+
+
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
         Debug.Log("OnEndDrag");
+        canvasGroup.alpha = 1f;
+        canvasGroup.blocksRaycasts = true;
     }
 
     public void OnPointerDown(PointerEventData eventData)
@@ -34,26 +52,5 @@ public class DragnDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
